0d912ba0d1c93fea50a78535b0155d75637f68d4
h0keun <dlghrms95@gmail.com>
2021-03-30 19:47:22 +0900

    AsyncTas

--- app/src/main/java/com/grow/naverboostcourse/MainActivity.java
+++ app/src/main/java/com/grow/naverboostcourse/MainActivity.java
@@ -3,28 +3,33 @@ package com.grow.naverboostcourse;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    //TextView textView;
 
     //int value = 0;
     //ValueHandler handler = new ValueHandler();
 
-    Handler handler2 = new Handler();
+    //Handler handler2 = new Handler();
+
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        textView = (TextView) findViewById(R.id.textView);
+        //textView = (TextView) findViewById(R.id.textView);
 
-        Button button = (Button) findViewById(R.id.button);
+/*        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,17 +61,71 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).start();
             }
-        });
+        });*/
 
-        Button button2 = (Button) findViewById(R.id.button2);
+        /*Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //textView.setText("현재 값 : "+value);
             }
+        });*/
+        progressBar = findViewById(R.id.progressBar);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressTask task = new ProgressTask();
+                task.execute("Start");
+            }
         });
 
     }
+
+    class ProgressTask extends AsyncTask<String, Integer, Integer> {
+
+        int value = 0;
+
+        @Override
+        protected Integer doInBackground(String... strings) {
+            while(true){
+                if(value > 100){
+                    break;
+                }
+
+                value += 1;
+
+                publishProgress(value);
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return value;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            progressBar.setProgress(values[0].intValue());
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            Toast.makeText(getApplicationContext(), "Complete", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
 
 /*    class BackgroundThread extends Thread {
         int value =0;
